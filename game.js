b3395802474bd0b3f0e3d1d0395dcc98bb2199a3
sk <dev.seakim@gmail.com>
2018-08-06 23:11:33 -0400

    fixed nextPlayer(); / working on action() and play() with inquirer

--- game.js
+++ game.js
@@ -6,159 +6,276 @@ var inquirer = require("inquirer");
  * OTHER PROPERTIES: bettingLimit
  */
 
- // bug: nextPlayer(), calculateValue() - for Ace
+// bug: calculateValue() - for Ace
  
- var playerSetup = require('./player.js');
- var cardSetup = require('./card.js');
  
- var gamePlay = (function () {
- 
-     /** SETUP SHOE OF DECKS */
-     var shoe = cardSetup.shoe;
-     cardSetup.shuffleDecks();
- 
-     /** SETUP DEALER AND PLAYERS */
-     var dealer = playerSetup.dealer,
-         players = playerSetup.players;
-     playerSetup.addPlayer("Sean",1000);
-     playerSetup.addPlayer("Mike",1000);
- 
-     /** SETUP CURRENT PLAYER */
-     var current = 0;
-     var currentPlayer = players[current];
- 
+var playerSetup = require('./player.js');
+var cardSetup = require('./card.js');
+
+var GamePlay = (function () {
+
+    /** SETUP SHOE OF DECKS */
+    var shoe = cardSetup.shoe;
+    cardSetup.shuffleDecks();
+
+    /** SETUP DEALER AND PLAYERS */
+    var dealer = playerSetup.dealer,
+        players = playerSetup.players;
+    playerSetup.addPlayer("Sean",1000);
+    playerSetup.addPlayer("James",1000);
+
+    /** SETUP CURRENT PLAYER */
+    var current = 0;
+    var currentPlayer = players[current];
+
      /**  */
  
-     /** CALCULATE HANDS VALUE */
-     var calculateCardValue = function (card) {
-         var cardValue = 0;
-         if (card[1] === 0) {
-             if (cardValue > 10) {
-                 console.log("22?")
-                 cardValue += 1;
-             } else {
-                 cardValue += 11;
-             }
-         } else if (card[1] >= 9 && card[1] <= 12) {
-             cardValue += 10;
-         } else {
-             cardValue += card[1] + 1;
-         }
-         return cardValue;
-     };
+    /** CALCULATE HANDS VALUE */
+    var calculateCardValue = function (card) {
+        var cardValue = 0;
+        if (card[1] === 0) {
+            if (cardValue > 10) {
+                console.log("22?")
+                cardValue += 1;
+            } else {
+                cardValue += 11;
+            }
+        } else if (card[1] >= 9 && card[1] <= 12) {
+            cardValue += 10;
+        } else {
+            cardValue += card[1] + 1;
+        }
+        return cardValue;
+    };
  
-     /** DEAL 2 CARDS TO THE PLAYERS AND THE DEALER */
-     var deal = function () {
-         for (var i = 0; i < 2; i++) {
-             for (var j = 0; j < playerSetup.players.length; j++) {
-                 var dealtCardtoPlayers = cardSetup.shoe.shift();
-                 playerSetup.players[j].handArea.push(dealtCardtoPlayers);
-                 playerSetup.players[j].handValue += calculateCardValue(dealtCardtoPlayers);
-             }
-             var dealtCardtoDealer = cardSetup.shoe.shift();
-             /** */
-             playerSetup.dealer.handArea.push(dealtCardtoDealer);
-             playerSetup.dealer.handValue += calculateCardValue(dealtCardtoDealer);
- 
-             // calculateCardValue does not work (asyncronous)
-             // playerSetup.dealer.handArea.push([0,0]);
-             // playerSetup.dealer.handValue += calculateCardValue([0,0]);
-             // console.log(playerSetup.dealer.handValue);
-         }
-     }
+    /** DEAL 2 CARDS TO THE PLAYERS AND THE DEALER */
+    var deal = function () {
+        for (var i = 0; i < 2; i++) {
+            for (var j = 0; j < playerSetup.players.length; j++) {
+                var dealtCardtoPlayers = cardSetup.shoe.shift();
+                playerSetup.players[j].handArea.push(dealtCardtoPlayers);
+                playerSetup.players[j].handValue += calculateCardValue(dealtCardtoPlayers);
+                // playerSetup.players[j].handValue = 21; // for testing 
+            }
+            var dealtCardtoDealer = cardSetup.shoe.shift();
+            
+        //  comment the below and test
+            playerSetup.dealer.handArea.push(dealtCardtoDealer);
+            playerSetup.dealer.handValue += calculateCardValue(dealtCardtoDealer);
+            // playerSetup.dealer.handValue = 21; // for testing 
+
+        //  calculateCardValue does not work (asyncronous)
+        //  uncomment the below and test
+            // playerSetup.dealer.handArea.push([0,0]);
+            // playerSetup.dealer.handValue += calculateCardValue([0,0]);
+            // console.log(playerSetup.dealer.handValue);
+        }
+    }
      
-     var bust = function () {
-         if (currentPlayer !== dealer) {
-             currentPlayer.betAmount = 0;
-             currentPlayer.handArea = [];
-             currentPlayer.handValue = 0;
-         }
-     }
+    var resetPlayerStatus = function () {
+        if (currentPlayer !== dealer) {
+            currentPlayer.betAmount = 0;
+            currentPlayer.handArea = [];
+            currentPlayer.handValue = 0;
+        }
+    }
  
-     /** USER ACTIONS: cutCard, bet, hit, doubleDown, stand, insurance, surrender */
-     var cutCard = function (num) {
-         cardSetup.shoe = cardSetup.shoe.concat(cardSetup.shoe.splice(0, num));
-     }
-     var bet = function (amt) {
-         currentPlayer.budget -= amt;
-         currentPlayer.betAmount = amt;
-     }
-     var insurance = function () {
-         insuranceAmt = currentPlayer.betAmount / 2;
-         if (dealer.handValue === 21) {
-             currentPlayer.budget += insuranceAmt * 2;
-         } else {
-             currentPlayer.budget -= insuranceAmt;
-         }
-     }
-     var hit = function () {
-         var dealtCardtoPlayers = cardSetup.shoe.shift();
-         currentPlayer.handArea.push(dealtCardtoPlayers);
-         currentPlayer.handValue += calculateCardValue(dealtCardtoPlayers);
-     }
-     var doubleDown = function () {
-         hit();
-         currentPlayer.budget -= currentPlayer.betAmount;
-         currentPlayer.betAmount += currentPlayer.betAmount;
-         toNextPlayer();
-     }
-     var stand = function () {
-         toNextPlayer();
-     }
-     var surrender = function () {
-         bust();
-         toNextPlayer();
-     }
+    /** USER ACTIONS */
+    /** BEFORE DEAL ACTIONS: cutCard, bet */
+    var cutCard = function (num) {
+        cardSetup.shoe = cardSetup.shoe.concat(cardSetup.shoe.splice(0, num));
+    }
+    var bet = function (amt) {
+        currentPlayer.budget -= amt;
+        currentPlayer.betAmount = amt;
+        if (current < players.length - 1) {
+            current += 1;
+            currentPlayer = players[current];
+        } else {
+            current = 0;
+            currentPlayer = players[current];
+        }
+    }
+
+    /** AFTER DEAL ACTIONS: hit, doubleDown, stand, insurance, surrender */
+    var insurance = function () {
+        insuranceAmt = currentPlayer.betAmount / 2;
+        if (dealer.handValue === 21) {
+            currentPlayer.budget += insuranceAmt * 2;
+        } else {
+            currentPlayer.budget -= insuranceAmt;
+        }
+    }
+    var hit = function () {
+        var dealtCardtoPlayers = cardSetup.shoe.shift();
+        currentPlayer.handArea.push(dealtCardtoPlayers);
+        currentPlayer.handValue += calculateCardValue(dealtCardtoPlayers);
+    }
+    var doubleDown = function () {
+        hit();
+        currentPlayer.budget -= currentPlayer.betAmount;
+        currentPlayer.betAmount += currentPlayer.betAmount;
+        toNextPlayer();
+    }
+    var stand = function () {
+        toNextPlayer();
+    }
+    var surrender = function () {
+        bust();
+        toNextPlayer();
+    }
  
-     // does not update currentPlayer
-     var toNextPlayer = function () {
-         if (current < players.length) {
-             current += 1;
-             currentPlayer = players[current];
-         } else {
-             currentPlayer = dealer;
-         }
-         return currentPlayer;
-     }
- 
-     return {
-         deal: deal,
-         dealer: dealer,
-         players: players,
-         shoe: shoe,
- 
-         /** USER ACTIONS */
-         cutCard: cutCard,
-         bet: bet,
-         hit: hit,
-         doubleDown: doubleDown,
-         stand: stand,
-         surrender: surrender,
-         insurance: insurance,
- 
-         /** MOVE TO NEXT PLAYER */
-         currentPlayer: currentPlayer,
-         toNextPlayer: toNextPlayer
-     }
+    // does not update currentPlayer
+    var toNextPlayer = function () {
+    //  console.log(currentPlayer);
+        if (current < players.length - 1) {
+            console.log("current",current);
+            console.log("players.length", players.length);
+            current += 1;
+            currentPlayer = players[current];
+        } 
+        else {
+            currentPlayer = dealer;
+        }
+    //  console.log(currentPlayer);
+        return currentPlayer;
+    }
+
+    var optionWithInsurance = {
+        name: "select", type: "list", message: "OPTIONS:",
+        choices: [ "HIT", "DOUBLEDOWN", "STAND", "INSURANCE", "SURRENDER" ]
+    }
+    var optionWithDoubleDownNoinsurance = {
+        name: "select", type: "list", message: "OPTIONS:",
+        choices: [ "HIT", "DOUBLEDOWN", "STAND", "SURRENDER" ]
+    }
+    var optionRegular = {
+        name: "select", type: "list", message: "OPTIONS:",
+        choices: [ "HIT", "STAND", "SURRENDER" ]
+    }
+    var play = function () {
+        if (currentPlayer.handArea.length === 2) {
+            /** PLAYER ACTIONS RIGHT AFTER DEAL */
+
+            if (currentPlayer.handValue === 21 && dealer.handValue !== 21) {
+                /** BLACKJACK */
+
+                currentPlayer.budget += currentPlayer.betAmount * 2.5;
+                resetPlayerStatus();
+
+            } else if (dealer.handArea[0][1] === 0) {
+                /** IF DEALER'S FACECARD === ACE, OPTION INCLUDES INSURANCE */
+
+                inquirer.prompt(
+                    optionWithInsurance
+                ).then( function(response) {
+                    console.log(response.select);
+                });
+            } else {
+                /** OPTION INCLUDES DOUBLEDOWN */
+
+                inquirer.prompt(
+                    optionWithDoubleDownNoinsurance
+                ).then( function(response) {
+                    console.log(response.select);
+                });
+            }
+        } else {
+            /** PLAYER ACTIONS AFTER FIRST ACTION */
+            inquirer.prompt(
+                optionRegular
+            ).then( function(response) {
+                console.log(response.select);
+            });
+        }
+
+        if (currentPlayer.handValue > 21) {
+            /** PLAYER BUST */
+            resetPlayerStatus();
+            toNextPlayer();
+        } else {
+            play();
+        }
+    }
+    var action = function (response) {
+        switch (response) {
+            case "HIT":
+                hit();
+                break;
+            case "STAND":
+                stand();
+                break;
+            case "DOUBLEDOWN":
+                doubleDown();
+                break;
+            case "SURRENDER":
+                surrender();
+                break;
+            case "INSURANCE":
+                insurance();
+                break;
+        }
+    }
+    
+    return {
+        init: function () {
+            /** FIRST USER GET TO CUT CARDS */
+            cutCard(30);
+
+            /** PLAYERS BET BEFORE DEAL */
+            // temporarily using for loop to automate the bet
+            for (var i = 0; i < players.length; i++) {
+                bet(50);
+            }
+
+            /** DEAL 2 CARDS TO THE EACH PLAYERS & THE DEALER */
+            deal();
+
+            console.log(players);
+            console.log(dealer);
+            console.log("\n");
+
+            // play();
+
+            
+            // console.log(players);
+
+
+            /** TESTING FOR CURRENT PLAYER */
+            // console.log(currentPlayer);
+            // toNextPlayer();
+            // console.log(currentPlayer);
+            // toNextPlayer();
+            // console.log(currentPlayer);
+            /** */
+        }
+    }
  })();
  
- /** SIMULATION TEST */
- // gamePlay.cutCard(10);
- gamePlay.deal();
+
+GamePlay.init();
+
+
+
+
+//  /** SIMULATION TEST */
+//  // gamePlay.cutCard(10);
+//  gamePlay.deal();
  
-     // does not update currentPlayer
- // console.log(gamePlay.toNextPlayer());
+//      // does not update currentPlayer
+//  // console.log(gamePlay.toNextPlayer());
  
- console.log(gamePlay.dealer);
- console.log(gamePlay.currentPlayer);
+//  console.log(gamePlay.dealer);
+// //  console.log(gamePlay.currentPlayer);
  
- gamePlay.bet(25);
+//  gamePlay.bet(25);
  
- // gamePlay.insurance();
- // gamePlay.hit();
- // gamePlay.doubleDown();
- // gamePlay.stand();
- // gamePlay.surrender();
+//  // gamePlay.insurance();
+//  // gamePlay.hit();
+//  // gamePlay.doubleDown();
+//  // gamePlay.stand();
+//  // gamePlay.surrender();
  
- gamePlay.toNextPlayer();
- console.log(gamePlay.currentPlayer);
+//  gamePlay.toNextPlayer();
+// //  console.log(gamePlay.toNextPlayer());
+//  console.log(gamePlay.currentPlayer);
